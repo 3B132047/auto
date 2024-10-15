da048af376d6c2a613ca26a0b09c0cb9f6308581
hello-world <7iuy6u@gmail.com>
2024-10-15 23:19:01 +0800

    mylog

--- public/index.php
+++ public/index.php
@@ -1,12 +1,28 @@
 <?php
+require'../vendor/autoload.php';
+
+//類別先import近來
+use Demo\Hello\Lara;
+use Demo\Hello\Someone;
+$lara=new Lara();
+$vincent=new Someone('Vincent');
+
+//類別為先import近來
+
+$mary=new \Demo\Hello\Someone('Mary');
+$john=new Demo\Hello\Someone('john');
+$hello=new Demo\HelloWorld();
+
+use Demo\HelloWorld as World;//類別另取別名
+$world=new World();
 
 use Monolog\Level;
-use Monolog\Logger;
+use Monolog\WISD;
 use Monolog\Handler\StreamHandler;
 
 // create a log channel
-$log = new Logger('name');
-$log->pushHandler(new StreamHandler('path/to/your.log', Level::Warning));
+$log = new WISD('name');
+$log->pushHandler(new StreamHandler('../log/my.log', Level::Warning));
 
 // add records to the log
 $log->warning('Foo');
